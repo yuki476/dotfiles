10137fd8c8ee3540df542f562c8dcf823878947e
yuki476 <yuki.476@gmail.com>
2019-02-24 16:46:20 +0900

    update script

--- dotfile_link.sh
+++ dotfile_link.sh
@@ -2,8 +2,14 @@
 # 設定用ディレクトリ作成
 mkdir ~/.vim
 mkdir ~/.vim/undo
+mkdir ~/.fonts
 
 # シンボリックリンク作成
 ln -sf ~/dotfiles/.vimrc ~/
 ln -sf ~/dotfiles/cheatsheet.md ~/.vim/cheatsheet.md
 ln -sf ~/dotfiles/colors/ ~/.vim/colors
+
+# フォントを追加
+cp ~/dotfiles/fonts/*.ttf ~/.fonts
+fc-cache -frv
+
